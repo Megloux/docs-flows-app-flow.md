a3148d87e1d79a96e54382f8461fbb65e8d17bf3
Megloux <megbowen.21@icloud.com>
2025-01-28 21:28:34 -0500

    Add initial exercise categories data

---
+++ src/data/categories/data.ts
@@ -0,0 +1,14 @@
+import { Category } from './types';
+
+export const categories: Category[] = [
+  {
+    id: 'abs',
+    name: 'Abdominal Exercises',
+    description: 'Core strengthening exercises focusing on the abdominal muscles'
+  },
+  {
+    id: 'obliques',
+    name: 'Oblique Exercises',
+    description: 'Exercises targeting the side abdominal muscles'
+  }
+];
